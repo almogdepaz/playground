7c5a5ca97744b3608312adaf0b482ac328053634
almog <almogdepaz@gmail.com>
2022-02-09 18:41:30 +0200

    add bancor contract

--- cmd/badger/main.go
+++ cmd/badger/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"math/big"
 	"price_monitor/badger"
+	"price_monitor/bancor"
 	uniswap "price_monitor/uniswap"
 	"price_monitor/util"
 
@@ -24,25 +25,24 @@ func init() { Client = util.GetClient(node) }
 
 func main() {
 	amount := 10
-	uni_pool := common.HexToAddress("0xE86204c4eDDd2f70eE00EAd6805f917671F56c52")   //Uniswap WBTC/DIGG LP (UNI-V2)
-	sushi_pool := common.HexToAddress("0x9a13867048e01c663ce8ce2fe0cdae69ff9f35e3") //Sushiswap WBTC/DIGG LP (UNI-V2)
+	digg_contract := common.HexToAddress("0x798d1be841a82a273720ce31c822c61a67a601c3") //digg
+	wbtc_contract := common.HexToAddress("0x2260fac5e5542a773aa44fbcfedf7c193bc2c599") //wbtc
 
-	uni_pair, err := uniswap.NewUniswapv2pairCaller(uni_pool, Client)
-	if err != nil {
-		log.Panic(fmt.Sprintf("Failed to instantiate pair caller: %v\n", err))
-	}
-	uni_res := FetchPoolStatsUniswap(uni_pair, amount)
+	// uni
+	uni_pool := common.HexToAddress("0xE86204c4eDDd2f70eE00EAd6805f917671F56c52") //Uniswap WBTC/DIGG LP (UNI-V2)
+	uni_res := FetchPoolStatsUniswap(uni_pool, amount)
 	fmt.Printf("\nUniswap WBTC/DIGG amount in %v ammount out %v", amount, uni_res)
 
-	sushi_pair, err := uniswap.NewUniswapv2pairCaller(sushi_pool, Client)
-	if err != nil {
-		log.Panic(fmt.Sprintf("Failed to instantiate pair caller: %v\n", err))
-	}
-	sushi_res := FetchPoolStatsUniswap(sushi_pair, amount)
-	fmt.Printf("\nSushiswap WBTC/DIGG amount in %v ammount out %v", amount, sushi_res)
+	// sushi
+	sushi_pool := common.HexToAddress("0x9a13867048e01c663ce8ce2fe0cdae69ff9f35e3") //Sushiswap WBTC/DIGG LP (UNI-V2)
+	sushi_res := FetchPoolStatsUniswap(sushi_pool, amount)
+	fmt.Printf("\nWBTC/DIGG amount in %v ammount out %v", amount, sushi_res)
 
+	// bancor
+	bancor_contract := common.HexToAddress("0x2F9EC37d6CcFFf1caB21733BdaDEdE11c823cCB0") //bancor contract
+	bancor_res := FetchPoolStatsBancor(bancor_contract, wbtc_contract, digg_contract, amount)
+	fmt.Printf("\nWBTC/DIGG amount in %v ammount out %v", amount, bancor_res)
 	addr2 := common.HexToAddress("0x0F92Ca0fB07E420b2fED036A6bB023c6c9e49940") //badger contract
-
 	badger_caller, err := badger.NewBadgerCaller(addr2, Client)
 	if err != nil {
 		log.Panic(fmt.Sprintf("Failed to instantiate pair caller: %v\n", err))
@@ -51,12 +51,32 @@ func main() {
 	if err != nil {
 		log.Panic(fmt.Sprintf("Failed to instantiate pair caller: %v\n", err))
 	}
-	fmt.Printf("\nBadger digg share price %v", price)
+	fmt.Printf("\nBadger digg price %v", price)
+}
+
+func FetchPoolStatsBancor(bancor_contract common.Address, wbtc_contract common.Address, digg_contract common.Address, amount int) *big.Int {
+	bancor_caller, err := bancor.NewBancorCaller(bancor_contract, Client)
+	if err != nil {
+		log.Panic(fmt.Sprintf("Failed to create bancor caller: %v\n", err))
+	}
+	path, err := bancor_caller.ConversionPath(&bind.CallOpts{Context: context.TODO()}, wbtc_contract, digg_contract)
+	if err != nil {
+		log.Panic(fmt.Sprintf("Failed to get conversion path: %v\n", err))
+	}
+	bancor_res, err := bancor_caller.RateByPath(&bind.CallOpts{Context: context.TODO()}, path, big.NewInt(int64(amount)))
+	if err != nil {
+		log.Panic(fmt.Sprintf("Failed to get rate for path: %v\n", err))
+	}
+	return bancor_res
 }
 
 // amount - the amount of token0 to send
 // returns the recived amount of token1 given the input
-func FetchPoolStatsUniswap(pair *uniswap.Uniswapv2pairCaller, amount_in int) *big.Float {
+func FetchPoolStatsUniswap(pool common.Address, amount_in int) *big.Float {
+	pair, err := uniswap.NewUniswapv2pairCaller(pool, Client)
+	if err != nil {
+		log.Panic(fmt.Sprintf("Failed to instantiate pair caller: %v\n", err))
+	}
 	token0, _ := pair.Token0(&bind.CallOpts{Context: context.TODO()})
 	token1, _ := pair.Token1(&bind.CallOpts{Context: context.TODO()})
 	amount1, err := uniswap.GetExchangeAmount(pair, big.NewFloat(float64(amount_in)), token0, token1)
